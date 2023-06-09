784abe408d190d09f303c8010110afed641ced37
Roreke <rooo28@hotmail.com>
2023-06-09 11:48:18 -0300

    test

---
+++ tests/testthat/test-shinytest2.R
@@ -0,0 +1,8 @@
+library(shinytest2)
+
+test_that("{shinytest2} recording: test2", {
+  app <- AppDriver$new(variant = platform_variant(), name = "test2", height = 1161, 
+      width = 2098)
+  app$expect_screenshot()
+})
+
